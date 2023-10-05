d005a61647734487f9f4bb00dd005e7fa337c290
Lana <lolhowko@gmail.com>
2023-10-05 17:59:09 +0300

    add 2 components.js

---
+++ src/components/playlist.js
@@ -0,0 +1,12 @@
+export function playist() {
+    return(
+        <div class="sidebar__item">
+        <a class="sidebar__link" href="#">
+          <img
+            class="sidebar__img"
+            src="img/playlist02.png"
+            alt="day's playlist" />
+        </a>
+      </div>
+    )
+}
---
+++ src/components/searchBlock.js
@@ -0,0 +1,14 @@
+export function searchBlock() {
+    return(
+        <div class="centerblock__search search">
+        <svg class="search__svg">
+          <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
+        </svg>
+        <input
+          class="search__text"
+          type="search"
+          placeholder="Поиск"
+          name="search" />
+      </div>
+    )
+}
