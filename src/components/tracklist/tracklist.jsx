e414823e9678c2d579ec880beaf8c3ea149717dd
Lana <lolhowko@gmail.com>
2023-10-18 18:16:15 +0300

    add styles to tracklist

--- src/components/tracklist/tracklist.jsx
+++ src/components/tracklist/tracklist.jsx
@@ -1,32 +1,32 @@
 import { FilterBlock } from "../filterBlock/filterBlock";
 import { SearchBlock } from "../searchBlock/searchBlock";
 import { Track } from "../track/track";
-import './tracklist.css'
+import * as S from './tracklist.styles'
 
 
 export function TrackList() {
     return (
-        <div className="main__centerblock centerblock">
+        <S.MainCenterblock className="centerblock">
 
         <SearchBlock />
         
-        <h2 className="centerblock__h2">Треки</h2>
+        <S.CenterblockH2>Треки</S.CenterblockH2>
 
         <FilterBlock />
 
-        <div className="centerblock__content">
-          <div className="content__title playlist-title">
-            <div className="playlist-title__col col01">Трек</div>
-            <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
-            <div className="playlist-title__col col03">АЛЬБОМ</div>
-            <div className="playlist-title__col col04">
-              <svg className="playlist-title__svg" alt="time">
+        <S.CenterblockContent>
+          <S.ContentTittle>
+            <S.Col01>Трек</S.Col01>
+            <S.Col02>ИСПОЛНИТЕЛЬ</S.Col02>
+            <S.Col03>АЛЬБОМ</S.Col03>
+            <S.Col04>
+              <S.ContentTittleSvg alt="time">
                 <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
-              </svg>
-            </div>
-          </div>
+              </S.ContentTittleSvg>
+            </S.Col04>
+          </S.ContentTittle>
           
-          <div className="content__playlist playlist">
+          <S.ContentPlaylist className="playlist">
 
 
             <Track />
@@ -34,9 +34,9 @@ export function TrackList() {
             <Track />
 
 
-          </div>
-        </div>
-      </div>
+          </S.ContentPlaylist>
+        </S.CenterblockContent>
+      </S.MainCenterblock>
     )
 }
 
