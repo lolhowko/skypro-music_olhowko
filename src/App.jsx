710b70a6c7cda75515d36693cf71802f7cc19836
Lana <lolhowko@gmail.com>
2023-10-20 14:45:16 +0300

    relocate Main page from app to MainTrack

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
-
-import { AudioPlayer } from "./components/audioPlayer/audioPlayer";
-import  { NavMenu } from './components/navMenu/navMenu';
-import { SideBar } from "./components/sideBar/sideBar";
-import { TrackList } from "./components/tracklist/tracklist";
-import * as S from './app.styles'
-import { GlobalStyle } from "./global.styles";
+import { AppRoutes } from "./routes";
 
 
 function App() {
     return (
       <>
-      <GlobalStyle />
       <div className="App">
-        <S.Wrapper>
-          <S.Container>
-            <S.Main>
 
-            <NavMenu />
-            <TrackList />
-            <SideBar /> 
-            <AudioPlayer />
-
-            </S.Main>
-            
-            <footer className="footer"></footer>
-          </S.Container>
-        </S.Wrapper>
+        <AppRoutes />
   
       </div>
       </>
--- src/pages/main-tracks/index.jsx
+++ src/pages/main-tracks/index.jsx
@@ -1,7 +1,29 @@
+import { AudioPlayer } from "../../components/audioPlayer/audioPlayer"
+import  { NavMenu } from '../../components/navMenu/navMenu';
+import { SideBar } from "../../components/sideBar/sideBar";
+import { TrackList } from "../../components/tracklist/tracklist";
+import { GlobalStyle } from "../../global.styles";
+import * as S from '../../app.styles'
+
 export const MainTracks = () => {
     return (
-        <div>
-            Main Tracks
-        </div>
+        <>
+        <GlobalStyle />
+
+        <S.Wrapper>
+          <S.Container>
+            <S.Main>
+
+            <NavMenu />
+            <TrackList />
+            <SideBar /> 
+            <AudioPlayer />
+
+            </S.Main>
+            
+            <footer className="footer"></footer>
+          </S.Container>
+        </S.Wrapper>
+        </>
     )
 }
