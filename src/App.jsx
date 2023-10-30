730e66a96aebc3ad2a73b931d79267ceecad5707
Lana <lolhowko@gmail.com>
2023-10-30 15:48:38 +0300

    add preventDefault

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from 'react'
 import { AppRoutes } from './routes'
+import { useNavigate } from 'react-router-dom';
 
 function App() {
 
   const [user, setUser] = useState(false)
 
-  const handleLogin = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+
     localStorage.setItem('user', 'true')
     const curentLocalStorage = localStorage.getItem('user')
     console.log(curentLocalStorage)
     setUser(curentLocalStorage)
+
   }
+
 
   return (
     <>
