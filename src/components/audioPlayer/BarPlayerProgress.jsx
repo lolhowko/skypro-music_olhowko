1cbf23e054dc2a5ad1a376e4f341e133a5ab87ed
Lana <lolhowko@gmail.com>
2023-11-06 13:44:53 +0300

    add BarPlayeerProgress

--- src/components/audioPlayer/BarPlayerProgress.jsx
+++ src/components/audioPlayer/BarPlayerProgress.jsx
@@ -1,24 +1,23 @@
+import { useEffect } from 'react'
 import { getDurationAudio } from '../../utilitis/getDuration'
 import * as S from './audioPlayer.styles'
 
-export function BarPlayerProgress({ duration, timeProgress, audioRef }) {
-  return (
-    <>
-      <S.BarPlayerProgressTime>
-        {getDurationAudio(timeProgress)} /{getDurationAudio(duration)}
-      </S.BarPlayerProgressTime>
-      <S.BarPlayeerProgress
-        type="range"
-        onChange={(e) => {
-          audioRef.current.currentTime = e.target.value
-        }}
-        min={0}
-        max={duration}
-        value={timeProgress}
-        step={0.1}
-      />
-    </>
-  )
-}
-
-export default BarPlayerProgress
+export default function BarPlayerProgress({ duration, timeProgress, audioRef }) {
+    return (
+      <>
+        <S.BarPlayerProgressTime>
+          {getDurationAudio(timeProgress)} /{getDurationAudio(duration)}
+        </S.BarPlayerProgressTime>
+        <S.StyledProgressInput
+          type="range"
+          onChange={(e) => {
+            audioRef.current.currentTime = e.target.value;
+          }}
+          min={0}
+          max={duration}
+          value={timeProgress}
+          step={0.1}
+        />
+      </>
+    );
+  }
