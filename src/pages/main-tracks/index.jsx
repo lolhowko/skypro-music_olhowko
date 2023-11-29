3a44e0e83c3b74cdd4ba35a598d7a50a5387d662
Lana <lolhowko@gmail.com>
2023-11-29 13:59:59 +0300

    add change to mainPages

--- src/pages/main-tracks/index.jsx
+++ src/pages/main-tracks/index.jsx
@@ -1,60 +1,83 @@
-import { AudioPlayer } from "../../components/audioPlayer/audioPlayer"
-import  { NavMenu } from '../../components/navMenu/navMenu';
-import { SideBar } from "../../components/sideBar/sideBar";
-import { TrackList } from "../../components/tracklist/tracklist";
-import { GlobalStyle } from "./global.styles";
-import * as S from '../../app.styles';
-import { useEffect, useState } from "react";
-import { getTracksAll } from "../../api";
+import { AudioPlayer } from '../../components/audioPlayer/audioPlayer'
+import { NavMenu } from '../../components/navMenu/navMenu'
+import { SideBar } from '../../components/sideBar/sideBar'
+import { TrackList } from '../../components/tracklist/tracklist'
+import { GlobalStyle } from './global.styles'
+import * as S from '../../app.styles'
+import { useEffect, useState } from 'react'
+import { getTracksAll } from '../../api'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  allTracksSelector,
+  currentTrackSelector,
+  shuffleAllTracksSelector,
+  shuffleSelector,
+} from '../../store/selectors/indexSelectors'
+import { setAllTracks, setCurrentTrack } from '../../store/slices/slices'
 
 export const MainTracks = () => {
+  const dispatch = useDispatch()
 
-  const [isLoading, setLoading] = useState(false);
-  const [tracks, setTracks] = useState([]);
-  const [currentTrack, setCurrentTrack] = useState(null);
-  const handleCurrentTrack = (track) => setCurrentTrack(track);
+  const tracks = useSelector(allTracksSelector)
+  // const [tracks, setTracks] = useState([]);
 
+  const currentTrack = useSelector(currentTrackSelector)
+  // const [currentTrack, setCurrentTrack] = useState(null);
+  // const handleCurrentTrack = (track) => setCurrentTrack(track);
+
+  const shuffle = useSelector(shuffleSelector)
+  const shuffleAllTracks = useSelector(shuffleAllTracksSelector)
+  const arrayTracksAll = shuffle ? shuffleAllTracks : tracks
+
+  const [isLoading, setLoading] = useState(false)
   const [tracksError, setTracksError] = useState(null)
 
-
-  useEffect(() => {
-  if (!isLoading) {
-    const timer = setTimeout(() => {
-      setLoading(true)
-    }, 5000)
-
-    return () => clearTimeout(timer);
+  const handleCurrentTrack = (track) => {
+    const indexCurrentTrack = arrayTracksAll.indexOf(track)
+    dispatch(setCurrentTrack({ track, indexCurrentTrack }))
   }
-  }, [isLoading]);
-
 
   useEffect(() => {
-    getTracksAll().then((track) => {
-      setTracks(track);
-    }). catch((error) => {
-      setLoadingTrackError(error.message)
-    })
-  }, []);
+    if (!isLoading) {
+      const timer = setTimeout(() => {
+        setLoading(true)
+      }, 5000)
 
+      return () => clearTimeout(timer)
+    }
+  }, [isLoading])
 
   useEffect(() => {
-    async function getTracks (){
-   try {
-     setLoading (true);//состояние загрузки началось
-     setTracksError(null);
-     await getTracksAll().then((tracks) => {
-     setTracks(tracks);
-   })//получение из апи треков
-   } catch(error) {
-     setTracksError(error.message)//если ошибка
-   } finally{
-     setLoading(false)//состояние загрузки закончилось после получения данных из апи
-   }
-   
-     }
-     getTracks();
-   }, [])
+    getTracksAll()
+      .then((track) => {
+        // setTracks(track);
+        dispatch(setAllTracks(track))
+      })
+      .catch((error) => {
+        setTracksError(error.message)
+        console.log(error.message)
+      })
+  }, [])
 
+  useEffect(() => {
+    async function getTracks() {
+      try {
+        setLoading(true) //состояние загрузки началось
+        setTracksError(null)
+        await getTracksAll().then((tracks) => {
+          // setTracks(tracks)
+          dispatch(setAllTracks(tracks))
+        }) //получение из апи треков
+      } catch (error) {
+        setTracksError(error.message) //если ошибка
+        console.log(error.message)
+
+      } finally {
+        setLoading(false) //состояние загрузки закончилось после получения данных из апи
+      }
+    }
+    getTracks()
+  }, [])
 
   //  useEffect(() => {
   //   getTracksAll().then((track) => {
@@ -65,34 +88,32 @@ export const MainTracks = () => {
   //   })
   // }, []);
 
-
-    return (
-      <>
+  return (
+    <>
       <GlobalStyle />
 
       <S.Wrapper>
         <S.Container>
           <S.Main>
+            <NavMenu />
 
-              <NavMenu />
-            
-              <TrackList 
+            <TrackList
               isLoading={isLoading}
               tracks={tracks}
               handleCurrentTrack={handleCurrentTrack}
               tracksError={tracksError}
-              />
+            />
 
-              <SideBar isLoading={isLoading}/> 
+            <SideBar isLoading={isLoading} />
 
-              {currentTrack && 
-              <AudioPlayer isLoading={isLoading} currentTrack={currentTrack}/>}
-
+            {currentTrack && (
+              <AudioPlayer isLoading={isLoading} currentTrack={currentTrack} />
+            )}
           </S.Main>
-            
+
           <footer className="footer"></footer>
         </S.Container>
       </S.Wrapper>
-      </>
-    )
+    </>
+  )
 }
--- src/store/indexStore.js
+++ src/store/indexStore.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import tracksReducer from "../reducers/indexReducers";
 import tracksReducer from "./slices/slices";
 
 export const store = configureStore({
