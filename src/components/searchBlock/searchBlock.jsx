a892aa73625698a82c9f8e0ba519dc3f15ec944c
Lana <lolhowko@gmail.com>
2024-01-10 14:32:10 +0300

    add search

--- src/components/searchBlock/searchBlock.jsx
+++ src/components/searchBlock/searchBlock.jsx
@@ -1,19 +1,30 @@
 // import './searchBlock.css'
+import { useDispatch } from 'react-redux'
 import * as S from './searchBlock.styles'
-
+import { setFilterPlaylist } from '../../store/slices/slices'
 
 export function SearchBlock() {
-    return(
-        <S.CenterblockSearch className="search">
-        <S.SearchSvg>
-          <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
-        </S.SearchSvg>
-        <S.SearchText
-          type="search"
-          placeholder="Поиск"
-          name="search" />
-      </S.CenterblockSearch>
-    )
+  const dispatch = useDispatch()
+  
+  return (
+    <S.CenterblockSearch className="search">
+      <S.SearchSvg>
+        <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
+      </S.SearchSvg>
+      <S.SearchText
+        type="search"
+        placeholder="Поиск"
+        name="search"
+        onChange={(e) => {
+          dispatch(
+            setFilterPlaylist({
+              search: e.target.value,
+            })
+          )
+        }}
+      />
+    </S.CenterblockSearch>
+  )
 }
 
 export default SearchBlock
